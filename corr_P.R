1bd9a93277b8bd26f6df146473661ce428814954
Rental <Rental>
2016-02-03 14:08:42 -0500

    made script fully interchangable between HIC and BON sites; outputs pdf plots of summaries

--- corr_P.R
+++ corr_P.R
@@ -3,7 +3,7 @@ library(dplR)
 
 
 #read in whole ring width file for site
-Bonanza <- read.tucson("./cofecha/BONall.rwl", header = T)
+Bonanza <- read.tucson("./cofecha/BON_out/BONall.rwl", header = T)
 Hickory <- read.tucson ("./cofecha/HICall.rwl", header = T)
 
 
@@ -54,7 +54,7 @@ summary(site.code.rwi)
 
 
 site.code.crn <- chron(site.code.rwi, prefix = paste(site.code))
-crn.plot(site.code.crn, add.spline = TRUE)
+site.cron.plot<- crn.plot(site.code.crn, add.spline = TRUE)
 site.code.stats <- rwl.stats(site)
 
 #add year to dataframe
@@ -116,15 +116,24 @@ temp.MN <- merge(temp, site.code.crn, by = "Year")
 site.code.Tcors <- cor(temp.MN[,2:13], temp.MN[,14], use = "pairwise.complete.obs")
 #plot the correlations by month 
 barplot(t(site.code.Tcors), main = "site Correlation with milan Temperature.")
+##################################################
+#################################################
+################################################
+################################################3
 
 
-##
+
+
+
+###########################################################
 #now using climate division data from west central minnesota
 if(site.code == "BON"){
 MNcd.clim <- read.csv("WestCenMNcd.csv")
 } else{
   MNcd.clim <- read.csv("IL_cd.csv")
 }
+
+
 
 keeps <- c("Year", "Month",  "PCP")
 keepst <- c("Year", "Month",  "TMAX")
@@ -145,6 +154,9 @@ MNtmin.df[MNtmin.df == -9999]<- NA
 MNpdsi.df<- MNcd.clim[,keepspdsi]
 MNpdsi.df[MNpdsi.df == -9999]<- NA
 #for precipitation
+
+
+
 
 library(plyr)
 total.p <- aggregate(PCP~Year + Month, data=MNp.df, FUN=sum, na.rm = T) 
@@ -193,7 +205,7 @@ site.code.Tmaxprev<- cor(temp.MN[1:120,2:13], temp.MN[2:121,14], use = "pairwise
 
 #plot the correlations by month 
 barplot(t(site.code.Tcors), ylim = c(-0.25, 0.5), main = paste(site.code,"Correlation with Maximum Temperature"))
-barplot(t(site.code.Tmaxprev), ylim = c(-0.25, 0.5), main = paste(site.code, "Correlation with Maximum Temperature"))
+barplot(t(site.code.Tmaxprev), ylim = c(-0.25, 0.5), main = paste(site.code, "Correlation with previous year Maximum Temperature"))
 
 #now with Tmin
 min.t <- aggregate(TMIN~Year + Month, data=MNtmin.df, FUN=sum, na.rm = T) 
@@ -274,9 +286,41 @@ HIC1424 <- detrend.series(site[,5], y.name = "HIC1424", method = "Spline",
 HIC1896 <- detrend.series(site[,6], y.name = "HIC1896", method = "Spline",
                           verbose= TRUE)
 
+
+
+#for bonanza prairie #bonanaza has 12 variables
+BON411a <- detrend.series(Bonanza[,1],y.name = "BON411a", method = "Spline",
+                          verbose= TRUE)
+BON511a <- detrend.series(Bonanza[,2],y.name = "BON511a", method = "Spline",
+                          verbose= TRUE)
+BON611a <- detrend.series(Bonanza[,3],y.name = "BON611a", method = "Spline",
+                          verbose= TRUE)
+BON711a <- detrend.series(Bonanza[,4],y.name = "BON711a", method = "Spline",
+                          verbose= TRUE)
+BON811a <- detrend.series(Bonanza[,5],y.name = "BON811a", method = "Spline",
+                          verbose= TRUE)
+BON911a <- detrend.series(Bonanza[,6],y.name = "BON911a", method = "Spline",
+                          verbose= TRUE)
+BON1011a <- detrend.series(Bonanza[,7],y.name = "BON1011a", method = "Spline",
+                          verbose= TRUE)
+BON1111a <- detrend.series(Bonanza[,8],y.name = "BON1111a", method = "Spline",
+                          verbose= TRUE)
+BON1211a <- detrend.series(Bonanza[,9],y.name = "BON1211a", method = "Spline",
+                          verbose= TRUE)
+BON1311a <- detrend.series(Bonanza[,10],y.name = "BON1311a", method = "Spline",
+                          verbose= TRUE)
+BON1411a <- detrend.series(Bonanza[,11],y.name = "BON1411a", method = "Spline",
+                          verbose= TRUE)
+
+
+#if statement that determines site detrending to use for corrplots
+if(site.code == "HIC"){
 all.detrended <- data.frame(Year = as.numeric(row.names(site)),HIC1180, HIC1349, HIC1396,
-                            HIC1398, HIC1424, HIC1896)
- 
+                            HIC1398, HIC1424, HIC1896)}else{
+all.detrended <- data.frame(Year = as.numeric(row.names(site)),BON411a, BON511a, BON611a, BON711a, 
+                            BON811a, BON911a, BON1011a, BON1111a, BON1211a,BON1311a, BON1411a)
+                            }
+
 #renaming and reformatting climate variables
 drought <- data.frame(drought)
 colnames(drought) <- c("Year", "Jan", "Feb", "Mar", "Apr",
@@ -286,6 +330,17 @@ temp <- data.frame(temp)
 colnames(temp) <- c("Year", "Jan", "Feb", "Mar", "Apr",
                     "May", "Jun", "Jul", "Aug", "Sep", 
                     "Oct", "Nov", "Dec")
+tmax.p <- data.frame(Year = temp[2:121,1], 
+                       temp[1:120,2:13])
+colnames(tmax.p) <- c("Year", "pJan", "pFeb", "pMar", "pApr",
+                      "pMay", "pJun", "pJul", "pAug", "pSep", 
+                      "pOct", "pNov", "pDec")
+full.tmax <- merge(tmax.p, temp, by = "Year")
+
+tmax.series<- merge(all.detrended,full.tmax, by = "Year")
+
+M.tmax <- cor(tmax.series, use = "pairwise.complete.obs")
+
 
 temp.min<- data.frame(temp.min)
 colnames(temp.min) <- c("Year", "Jan", "Feb", "Mar", "Apr",
@@ -336,14 +391,54 @@ p.values <- flattenSquareMatrix(cor.prob(pr.series))
 library(lattice)
 levelplot(M)
 corrplot(M, type = "upper", method = "circle")
-M2 <- M[8:31,2:7] #reduce size of matrix to exclude correlations of the same variables
+
+if(site.code == "HIC"){
+M2 <- M[8:31,2:7]
+M.tmax2 <- M.tmax[8:31,2:7] ##reduce size of matrix to exclude correlations of the same variables
+}else{M2 <- M[13:nrow(M),2:12]
+M.tmax2 <- M.tmax[13:nrow(M.tmax),2:12] }
+
 
 corrplot(M2, method = "color")
-levelplot(M2)
+levelplot(M2 ,main=list('Precipitation',side=1,line=0.5))
 
 z.m <- melt(M2)
 
 #a ggplot way of visualizing
-ggplot(z.m, aes(Var1, Var2, fill = value)) + geom_tile() + 
-  scale_fill_gradient(low = "red",  high = "blue")
+precip.corrplot<- ggplot(z.m, aes(Var1, Var2, fill = value)) + geom_tile() + 
+  scale_fill_gradient(low = "red",  high = "blue")+
+  ylab("Tree Series") + xlab("Precpitation") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+##correlation for tmax
+#
+
+z.mt <- melt(M.tmax2)
+
+#a ggplot way of visualizing
+tmax.corrplot<- ggplot(z.mt, aes(Var1, Var2, fill = value)) + geom_tile() + 
+  scale_fill_gradient(low = "red",  high = "blue")+
+  ylab("Tree Serires") + xlab("Max. temperature") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+pdf(paste0(site.code,"_summary_feb3.pdf"))
+plot(site, plot.type= "spag")
+crn.plot(site.code.crn, add.spline = TRUE)
+#plot the correlations by month 
+barplot(t(site.code.Pcors),ylim = c(-0.25, 0.5), main = paste(site.code,"Correlation with Precip."))
+barplot(t(site.code.Pprev), ylim = c(-0.25, 0.5), main = paste(site.code, "Correlation with previous year Precip"))
+##
+barplot(t(site.code.Tcors), ylim = c(-0.25, 0.5), main = paste(site.code,"Correlation with Maximum Temperature"))
+barplot(t(site.code.Tmaxprev), ylim = c(-0.25, 0.5), main = paste(site.code, "Correlation with previous year Maximum Temperature"))
+
+site.codetmin.p <- barplot(t(site.code.Tmincors), ylim= c(-0.25, 0.5), main = paste(site.code, "Correlation with Minimum Temperature"))
+site.codetminprev.p<- barplot(t(site.code.Tminprev),ylim = c(-0.25, 0.5), main = paste(site.code, "Correlation with previous year Minimum Temperature"))
+
+PDSI.b<- barplot(t(site.code.PDSIcors),ylim = c(-0.25, 0.5), main = paste(site.code,"Correlation with PDSI"))
+pdsi.prev <- barplot(t(site.code.PDSIprev),ylim = c(-0.25, 0.5), main = paste(site.code, "Correlation with previous year PDSI"))
+
+precip.corrplot
+levelplot(M2,main=list('Precipitation',side=1,line=0.5),scales=list(x=list(rot=90)))
+tmax.corrplot
+levelplot(M.tmax2, ,main=list('Max. Temperature',side=1,line=0.5),scales=list(x=list(rot=90)))
+dev.off()
