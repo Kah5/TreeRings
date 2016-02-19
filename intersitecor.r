990e986a7d9eafecaa5030b98333354d50daac81
kheil <kheilman@nd.edu>
2016-02-19 14:17:43 -0700

    changed barplots to plot HIC and BON correlations side by side

--- intersitecor.r
+++ intersitecor.r
@@ -1,39 +1,74 @@
 #making pretty site level corrleation comparisons
+if(wood == "EW"){
+#EW
+HICp<- read.csv("HIC-EWPrecipcor.csv")
+BONp <- read.csv("BON-EWPrecipcor.csv")
+
+HICtmax <- read.csv("HIC-EWtmaxcor.csv")
+BONtmax <- read.csv("BON-EWtmaxcor.csv")
+
+HICtmin <- read.csv("HIC-EWtmincor.csv")
+BONtmin <- read.csv("BON-EWtmincor.csv")
+
+HICtavg <- read.csv("HIC-EWtavgcor.csv")
+BONtavg <- read.csv("BON-EWtavgcor.csv")
+
+HICpdsi <- read.csv("HIC-EWpdsicor.csv")
+BONpdsi <- read.csv("BON-EWpdsicor.csv")
+}else{if(wood == "LW"){
+  HICp<- read.csv("HIC-LWPrecipcor.csv")
+  BONp <- read.csv("BON-LWPrecipcor.csv")
+  
+  HICtmax <- read.csv("HIC-LWtmaxcor.csv")
+  BONtmax <- read.csv("BON-LWtmaxcor.csv")
+  
+  HICtmin <- read.csv("HIC-LWtmincor.csv")
+  BONtmin <- read.csv("BON-LWtmincor.csv")
+  
+  HICtavg <- read.csv("HIC-LWtavgcor.csv")
+  BONtavg <- read.csv("BON-LWtavgcor.csv")
+  
+  HICpdsi <- read.csv("HIC-LWpdsicor.csv")
+  BONpdsi <- read.csv("BON-LWpdsicor.csv")
+}else{
+#WW
+HICtmax <- read.csv("HIC-WWtmaxcor.csv")
+BONtmax <- read.csv("BON-WWtmaxcor.csv")
+
 HICp<- read.csv("HIC-WWPrecipcor.csv")
 BONp <- read.csv("BON-WWPrecipcor.csv")
+
+HICtmin <- read.csv("HIC-WWtmincor.csv")
+BONtmin <- read.csv("BON-WWtmincor.csv")
+
+HICtavg <- read.csv("HIC-WWtavgcor.csv")
+BONtavg <- read.csv("BON-WWtavgcor.csv")
+
+HICpdsi <- read.csv("HIC-WWpdsicor.csv")
+BONpdsi <- read.csv("BON-WWpdsicor.csv")
+}}
 
 months <- c("pJan", "pFeb","pMar", "pApr", "pMay", "pJun",
             "pJul", "pAug", "pSep", "pOct", "pNov", "pDec",
             "Jan", "Feb","Mar", "Apr", "May", "Jun",
             "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
-
+#create dataframes of each site together
 prcors<- data.frame(HIC = HICp$V1, 
           BON = BONp$V1,
            months = months)
-
-HICtmin <- read.csv("HIC-WWtmincor.csv")
-BONtmin <- read.csv("BON-WWtmincor.csv")
 
 tmincors<- data.frame(HIC = HICtmin$V1, 
                     BON = BONtmin$V1,
                     months = months)
 
-HICtmax <- read.csv("HIC-WWtmaxcor.csv")
-BONtmax <- read.csv("BON-WWtmaxcor.csv")
-
 tmaxcors<- data.frame(HIC = HICtmax$V1, 
                       BON = BONtmax$V1,
                       months = months)
-
-HICpdsi <- read.csv("HIC-WWpdsicor.csv")
-BONpdsi <- read.csv("BON-WWpdsicor.csv")
 
 pdsicors<- data.frame(HIC = HICpdsi$V1, 
                       BON = BONpdsi$V1,
                       months = months)
 
-HICtavg <- read.csv("HIC-WWtavgcor.csv")
-BONtavg <- read.csv("BON-WWtavgcor.csv")
 
 tavgcors<- data.frame(HIC = HICtavg$V1, 
                       BON = BONtavg$V1,
@@ -65,22 +100,64 @@ barplot(t(x),ylim= c(-0.4, 0.5), beside = TRUE, col = colours, las = 2, main = p
 box()
 }
 
-pdf("site.compare.pdf")
-make.barplot(prcors, "Precipitation")
-make.barplot(tavgcors, "Temperature")
-make.barplot(tmincors, "Max. Temperature")
-make.barplot(tmaxcors, "Min. Temperature")
-make.barplot(pdsicors, "PDSI")
+pdf(paste0(wood, "-site.compare.pdf"))
+make.barplot(prcors, paste0(wood, "-Precipitation"))
+make.barplot(tavgcors, paste0(wood,"-Temperature"))
+make.barplot(tmincors, paste0(wood,"Max. Temperature"))
+make.barplot(tmaxcors, paste0(wood,"Min. Temperature"))
+make.barplot(pdsicors, paste0(wood,"PDSI"))
 dev.off()
 
 
+# afunction to plot correlations of growth to climate in a barplot
+make.barplotHIC <- function(x,y){
+  x <- as.matrix(x[,1])
+  months <- c("pJan", "pFeb","pMar", "pApr", "pMay", "pJun",
+              "pJul", "pAug", "pSep", "pOct", "pNov", "pDec",
+              "Jan", "Feb","Mar", "Apr", "May", "Jun",
+              "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+  row.names(x)<- months
+  colours <- c("brown1")
+  barplot(t(x),ylim= c(-0.4, 0.5), beside = TRUE, col = colours, las = 2, main = paste(y))
+  legend("topleft", c("HICKORY GROVE"),cex=1.3, bty="n", fill = colours )
+  abline(h = critical.r(120))
+  abline(h = critical.r(120)*-1)
+  barplot(t(x),ylim= c(-0.4, 0.5), beside = TRUE, col = colours, las = 2, main = paste(y), add = T)
+  box()
+}
 
+make.barplotBON <- function(x,y){
+  x <- as.matrix(x[,2])
+  months <- c("pJan", "pFeb","pMar", "pApr", "pMay", "pJun",
+              "pJul", "pAug", "pSep", "pOct", "pNov", "pDec",
+              "Jan", "Feb","Mar", "Apr", "May", "Jun",
+              "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+  row.names(x)<- months
+  colours <- c("darkblue")
+  barplot(t(x),ylim= c(-0.4, 0.5), beside = TRUE, col = colours, las = 2, main = paste(y))
+  legend("topleft", c("Bonanza Prairie"),cex=1.3, bty="n", fill = colours )
+  abline(h = critical.r(120))
+  abline(h = critical.r(120)*-1)
+  barplot(t(x),ylim= c(-0.4, 0.5), beside = TRUE, col = colours, las = 2, main = paste(y), add = T)
+  box()
+}
 
-##########older code
-library(reshape2)
-prs.long<-melt(prcors,id.vars="months")
+#make separate barplots here
+pdf(paste0(wood ,"sidebysidesites.pdf"))
+par(mfrow = c(2,1))
+make.barplotHIC(prcors, paste0(wood,"-HIC Precipitation"))
+make.barplotBON(prcors, paste0(wood,"-BON Precipitation"))
+                
+make.barplotHIC(tavgcors, paste0(wood, "-HIC Precipitation"))
+make.barplotBON(tavgcors, paste0(wood, "-BON Precipitation"))
 
-g1<-ggplot(prs.long, aes(x = factor(months), y = value , 
-                       fill=factor(variable),color=factor(variable))) + 
-  geom_bar(stat = "identity", position=position_dodge(3)) 
-  
+make.barplotHIC(tmincors, paste0(wood,"-HIC Max. Temperature"))
+make.barplotBON(tmincors, paste0(wood,"-BON Max. Temperature"))
+
+make.barplotHIC(tmaxcors, paste0(wood,"-HIC Min. Temperature"))
+make.barplotBON(tmaxcors, paste0(wood,"-BON Min. Temperature"))
+
+make.barplotHIC(pdsicors, paste0(wood,"-HIC PDSI"))
+make.barplotBON(pdsicors, paste0(wood,"-BON PDSI"))
+
+dev.off()
