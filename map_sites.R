71c274f249908eaefa19f46ecced4620a54598ba
kah5 <kellyannheilman@gmail.com>
2016-06-08 16:48:41 -0400

    updated to use data from GHCN network

--- map_sites.R
+++ map_sites.R
@@ -19,7 +19,59 @@ IL.MCCD <- data.frame(IL.MCCD)
 priority <- rbind(priority, IL.MCCD)
 
 #use avg.alb from extract_PT.R
+#getting gridded climate data
+#need to clean this up
 
+precip.1900<- read.table("./data/precip_2014/precip.1900")
+precip.1901<- read.table("./data/precip_2014/precip.1901")
+precip.1902<- read.table("./data/precip_2014/precip.1902")
+precip.1903<- read.table("./data/precip_2014/precip.1903")
+precip.1904<- read.table("./data/precip_2014/precip.1904")
+precip.1905<- read.table("./data/precip_2014/precip.1905")
+precip.1906<- read.table("./data/precip_2014/precip.1906")
+precip.1907<- read.table("./data/precip_2014/precip.1907")
+precip.1908<- read.table("./data/precip_2014/precip.1908")
+precip.1909<- read.table("./data/precip_2014/precip.1909")
+precip.1910<- read.table("./data/precip_2014/precip.1910")
+precip.2010<- read.table("./data/precip_2014/precip.2010")
+
+Lat <- precip.1900[,2]
+Long <- precip.1900[,1]
+
+p.1900<- rowSums(precip.1900[,3:14], na.rm = TRUE)
+p.1901<- rowSums(precip.1901[,3:14], na.rm = TRUE)
+p.1902<- rowSums(precip.1902[,3:14], na.rm = TRUE)
+p.1903<- rowSums(precip.1903[,3:14], na.rm = TRUE)
+p.1904<- rowSums(precip.1904[,3:14], na.rm = TRUE)
+p.1905<- rowSums(precip.1905[,3:14], na.rm = TRUE)
+p.1906<- rowSums(precip.1906[,3:14], na.rm = TRUE)
+p.1907<- rowSums(precip.1907[,3:14], na.rm = TRUE)
+p.1908<- rowSums(precip.1908[,3:14], na.rm = TRUE)
+p.1909<- rowSums(precip.1909[,3:14], na.rm = TRUE)
+p.1910<- rowSums(precip.1910[,3:14], na.rm = TRUE)
+
+avg.p<- rowMeans(data.frame(p.1900,
+                            p.1901, 
+                            p.1902,
+                            p.1903, 
+                            p.1904, 
+                            p.1905, 
+                            p.1906, 
+                            p.1907,
+                            p.1908,
+                            p.1909, 
+                            p.1910))/11
+
+averages <- data.frame(Lat = Lat, 
+                       Long = Long, 
+                       avg = avg.p)
+
+coordinates(averages) <- ~Long + Lat
+gridded(averages) <- TRUE
+avg.rast <- raster(averages)
+projection(avg.rast) <- CRS("+init=epsg:4326")
+
+avg.alb <- projectRaster(avg.rast, crs='+init=epsg:3175')
 #plot(avg.alb)
 
 
