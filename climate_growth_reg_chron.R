a0f11ebff8cc19ffaeceb15c0df9a30dada6ce4d
kah5 <kellyannheilman@gmail.com>
2016-09-07 23:45:18 -0400

    added stc climate and chronologies

--- climate_growth_reg_chron.R
+++ climate_growth_reg_chron.R
@@ -208,6 +208,7 @@ molten.BON <- merge.clim.chron(MNwc.clim, Bonanza)
 molten.DES <- merge.clim.chron(MNwc.clim, Desoix)
 molten.PLE <- merge.clim.chron(WIsc.clim, Pleasant)
 molten.TOW <- merge.clim.chron(MNse.clim, Townsend)
+molten.STC <- merge.clim.chron(MNse.clim, StCroix)
 
 ###############################################################
 #using the treeclim R pacakge to calculate moving correlations#
@@ -322,91 +323,10 @@ clim.cor(MNwc.clim, Bonanza, 'Bonanza_Prairie_')
 clim.cor(MNwc.clim, Desoix, 'Desoix_')
 clim.cor(WIsc.clim, Pleasant, 'Pleasant_Valley_Conservancy_')
 clim.cor(MNse.clim, Townsend, 'Townsend_woods_')
-
-
-########################
-##read in STC climate
-##############################
-
-MNcd.clim <- read.csv("data/CDODiv2154347072867.csv")
-MNcd.clim$PCP <- MNcd.clim$PCP*25.54
-
-keeps <- c("Year", "Month",  "PCP")
-keepstavg <- c("Year", "Month", "TAVG")
-keepst <- c("Year", "Month",  "TMAX")
-keepstmin <- c("Year", "Month",  "TMIN")
-keepspdsi <- c("Year", "Month",  "PDSI")
-#create a dataset for Precip
-MNp.df <- MNcd.clim[,keeps]
-MNp.df[MNp.df == -9999]<- NA
-
-#for tmax
-MNt.df <- MNcd.clim[,keepst]
-MNt.df[MNt.df == -9999]<- NA
-
-#for tmin
-MNtmin.df<- MNcd.clim[,keepstmin]
-MNtmin.df[MNtmin.df == -9999]<- NA
-
-#for tavg
-MNtavg.df <- MNcd.clim[,keepstavg]
-MNtavg.df[MNtavg.df == -9999]<- NA
-
-MNpdsi.df<- MNcd.clim[,keepspdsi]
-MNpdsi.df[MNpdsi.df == -9999]<- NA
-#for precipitation
-
-
-total.p <- aggregate(PCP ~ Year + Month, data=MNp.df, FUN=sum, na.rm = T) 
-months <- 6:9
-MNpjja.df <- MNp.df[MNp.df$Month %in% months,]
-jja.p <- aggregate(PCP ~ Year, data = MNpjja.df, FUN = sum, na.rm = T)
-
-total.p <- aggregate(PCP ~ Year + Month, data=MNp.df, FUN=sum, na.rm = T) 
-may.p <- total.p[total.p$Month == 5, ]
-
-tavg.m <- aggregate(TAVG ~ Year + Month, data=MNtavg.df, FUN=sum, na.rm = T) 
-jun.tavg <- tavg.m[tavg.m$Month == 6,]
-
-tmin.m <- aggregate(TMIN ~ Year + Month, data = MNtmin.df, FUN = sum, na.rm = T)
-jun.tmin <- tmin.m[tmin.m$Month == 6, ]
-
-tmax.m <- aggregate(TMAX ~ Year + Month, data = MNt.df, FUN = sum, na.rm = T)
-jun.tmax <- tmax.m[tmax.m$Month == 6, ]
+clim.cor(MNse.clim, StCroix, 'StCroix_savanna_')
 
 
 
-
-#pr.yr <- aggregate(PCP ~ Year , data=MNp.df, FUN=sum, na.rm = T) 
-#plot(pr.yr[1:120,1], pr.yr[1:120,2], type = "l", xlab = "Year", ylab = "Annual Precip (mm)")
-
-
-#precip <- dcast(total.p, Year  ~ Month)
-annual.p <- aggregate(PCP~Year, data = MNp.df[1:1440,], FUN = sum, na.rm=T)
-annual.t <- aggregate(TAVG ~ Year, data = MNtavg.df[1:1440,], FUN = 'mean', na.rm=T)
-annual.mint <- aggregate(TMIN ~Year, data = MNtmin.df[1:1440,], FUN = 'mean', na.rm = T)
-annual.pdsi <- aggregate(PDSI ~ Year, data = MNpdsi.df[1:1440,], FUN = 'mean', na.rm = T)
-annual.pdsi.m <- aggregate(PDSI ~ Year + Month, data = MNpdsi.df[1:1440,], FUN = 'mean', na.rm = T)
-jul.pdsi <- annual.pdsi.m[annual.pdsi.m$Month == 7,] 
-
-annuals <- data.frame(Year = annual.p$Year, 
-                      PCP = annual.p$PCP,
-                      TMIN = annual.mint$TMIN,
-                      TAVG = annual.t$TAVG,
-                      PDSI = annual.pdsi$PDSI,
-                      MAY.p = may.p[1:120,]$PCP,
-                      JJA.p = jja.p[1:120,]$PCP,
-                      JUNTmin = jun.tmin[1:120,]$TMIN,
-                      JUNTavg = jun.tavg[1:120,]$TAVG, 
-                      JUNTmax = jun.tmax[1:120,]$TMAX,
-                      Jul.pdsi = jul.pdsi[1:120,]$PDSI)
-
-#merge annuals with stcroix
-annuals.STC <- merge(annuals, StCroix[c('Year', 'xxxstd', 'Site')], by = 'Year')
-molten.STC <- melt(annuals.STC, id = c('Year','Site', 'PCP', "TMIN", "TAVG","PDSI","MAY.p","JJA.p", 
-                                       "JUNTmin","JUNTavg","JUNTmax","Jul.pdsi"))
-################3
-#now merge all the molten frames together
 
 molten.full <- rbind(molten.HIC, molten.BON, molten.STC, #molten.PLE,
                      molten.TOW)
