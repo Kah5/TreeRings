f4a74a3f151f23ada159c7452c66e966a592eaad
Kah5 <kellyannheilman@gmail.com>
2019-01-12 17:55:56 -0500

    updating model summary with dic values

--- R/output_model_summaries.R
+++ R/output_model_summaries.R
@@ -8,4 +8,31 @@ summary.files <- list.files(pattern = ".csv$") # get all the data files
 summary.data <- lapply(summary.files, FUN = read.csv) 
 summary.data.df <- do.call(rbind, summary.data) # converte from list of df to df
 
-write.csv(summary.data.df ,"/Users/kah/Documents/TreeRings/outputs/growth_model/model_summary_full.csv", row.names = FALSE)
+# get DIC estimates:
+
+summary.files.DIC <- list.files(pattern = "DIC.rds$") # get all the data files
+summary.dic.DATA <- lapply(summary.files, FUN = readRDS)
+
+# get names of models:
+model.names <- stringr::str_remove(summary.files.DIC, "_DIC.rds")
+names(summary.dic.DATA) <- model.names
+
+
+# summarise DIC values using sums--need to do this & unlist the values:
+
+deviances <- lapply(summary.dic.DATA, FUN = function(x){sum(x$deviance)})
+penalties <- lapply(summary.dic.DATA, FUN = function(x){sum(x$penalty)})
+
+deviance.df <- data.frame(penalties = (unlist(penalties)), 
+           deviances = (unlist(deviances)))
+deviance.df$model <- rownames(deviance.df)
+
+# add together to get penalized deviances:
+deviance.df$penalized_deviance <- deviance.df$penalties + deviance.df$deviances
+
+
+summary.data.df.dev <- merge(summary.data.df, deviance.df, by = "model", all.x = TRUE)
+
+# merge with the summary.data.df dataframe:
+
+write.csv(summary.data.df.dev ,"/Users/kah/Documents/TreeRings/outputs/growth_model/model_summary_full.csv", row.names = FALSE)
