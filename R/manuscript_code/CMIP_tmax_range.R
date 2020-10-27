19bf27fc3f14ef6b44e6de2404f0ec9128d5753b
Kah5 <kellyannheilman@gmail.com>
2020-10-27 10:07:33 -0700

    updating code to include more of the climate model projections

--- R/manuscript_code/CMIP_tmax_range.R
+++ R/manuscript_code/CMIP_tmax_range.R
@@ -77,7 +77,7 @@ temp.files <- list.files("data/future_climate_tas_all/bcsd5/", pattern = ".nc")
 temp.ncs <- paste0(getwd(),"/data/future_climate_tas_all/bcsd5/", temp.files) # full filenames
 
 
-climate_output <- temp.ncs[2]
+climate_output <- temp.ncs
 
 nc <- nc_open(climate_output)
 variableofinterest <- names(nc$var) # get the variable of interest
@@ -89,7 +89,7 @@ lat <- ncvar_get(nc, varid = "latitude")
 lat <- ncvar_get(nc, varid = "latitude")
 #proj <- ncvar_get(nc, varid = "projection")
 time <- ncvar_get(nc, varid = "time")
-summary(360 - lon)
+summary(lon)
 summary(lat)
 nc$dim$time$units
 nc$dim$time$calendar
@@ -114,56 +114,209 @@ proj <- 1
 tas <- tave
 cov.data.ll <- tr_sites.lat.df
 
-extract.tmax.july <- function(proj, tas,  tr_sites.lat.df , nmonths ){
-# for the 1st model + projection, get raster lists of all the months
-# make a raster for each month 
-  
-  
-years <-  c(rep(2025:2098, each = 12),2099)
-
-months <- c(rep(1:12, 74), 1)
-ymonths <- 1:889
-julys <- ymonths[ months%in% "6"]
-
-for(m in julys){ 
-  rlist[[m]] <- raster(as.matrix(t(tas[,101:1,m,proj])), xmn = min(lon), xmx = max(lon), 
-                            ymn = min(lat) , ymx = max(lat) , 
-                            crs = CRS('+init=epsg:4269'))
+# set up a function to extract only the june tmax values from a single model run/projection
+extract.tmax.june <- function(proj, tas,  tr_sites.lat.df , nmonths ){
+    # for the 1st model + projection, get raster lists of all the months
+    # make a raster for each month 
+      
+      
+    years <-  c(rep(2025:2099, each = 12))
+    
+    months <- c(rep(1:12, 75))
+    ymonths <- 1:900
+    julys <- ymonths[ months%in% "6"]
+    rlist <- list()
+    for(m in 1:length(julys)){ 
+      rlist[[m]] <- raster(as.matrix(t(tas[,99:1,julys[m],proj])), xmn = min(lon), xmx = max(lon), 
+                                ymn = min(lat) , ymx = max(lat) , 
+                                crs = CRS('+init=epsg:4269'))
+    }
+    
+    rast.stack.tave <- stack(rlist)
+    
+    extracted.pts.tave <- data.frame(raster::extract(rast.stack.tave, tr_sites.lat.df[, c("coords.x1", "coords.x2")]))
+    
+    #ll.data <- as.data.frame(cov.data.ll)
+    extracted.pts.tave$lat <-tr_sites.lat.df$coords.x2 # get the lat and long
+    extracted.pts.tave$lon <-tr_sites.lat.df$coords.x1
+    
+    #system.time(removed.nas<- extracted.pts.tave[!rowSums(is.na(  extracted.pts.tave[cols])), ])
+    
+    # do the same for the extracted.pts.t
+    
+    
+    colnames(extracted.pts.tave)[1:75] <- c(paste0("tave_", 2025:2099, "_", c(rep(6, 74)) )) #, "tavet_2099_1") # note may need to change this to make more customizable
+    extracted.pts.tave.m <- melt(extracted.pts.tave, id.vars = c("lat", "lon"))
+    extracted.pts.tave.m$value <- ifelse(extracted.pts.tave.m$value >= 1e+20, NA, extracted.pts.tave.m$value)
+    ext.tave.sep <- extracted.pts.tave.m %>% tidyr::separate(variable, sep = "_", into = c("climate", "year", "month"))
+    colnames(ext.tave.sep)[6] <- "Tave"
+    
+    
+    
+    
+    
+    saveRDS(ext.tave.sep, paste0("data/future_climate_tas_all/rdsfiles/june_tmax_summary_proj_", proj, "_v1.rds"))
+    rm(rast.stack.tave)
 }
 
-rast.stack.tave <- stack(rlist)
 
-extracted.pts.tave <- data.frame(raster::extract(rast.stack.tave, tr_sites.lat.df[, c("coords.x1", "coords.x2")]))
+extract.tmax.june(proj = 2, tas = tas, tr_sites.lat.df = tr_sites.lat.df, nmonths = nmonths)
 
-#ll.data <- as.data.frame(cov.data.ll)
-extracted.pts.tave$lat <-tr_sites.lat.df$coords.x2 # get the lat and long
-extracted.pts.tave$lon <-tr_sites.lat.df$coords.x1
+# get a list of all the model runs and projections and separate based on rcp scenarios
+all.proj.names <- read.delim("data/future_climate_tas_all/bcsd5/Projections5.txt", header = FALSE)
+proj.allnum <- 1:length(all.proj.names$V1)
+# get the rcp values for all the projections
+all.proj.names$rcp <- substr(all.proj.names$V1, nchar(as.character(all.proj.names$V1))-4, nchar(as.character(all.proj.names$V1)))
 
-#system.time(removed.nas<- extracted.pts.tave[!rowSums(is.na(  extracted.pts.tave[cols])), ])
+all.proj.names$proj.allnum <- 1:length(all.proj.names$V1)
 
-# do the same for the extracted.pts.t
+# every 4th model is rcp 2.6
 
+rcp.2.6.index <- all.proj.names %>% filter(rcp %in% "rcp26") %>% select(proj.allnum)
 
-colnames(extracted.pts.tave)[1:74] <- c(paste0("tave_", 2025:2098, "_", c(rep(6, 74)) )) #, "tavet_2099_1") # note may need to change this to make more customizable
-extracted.pts.tave.m <- melt(extracted.pts.tave, id.vars = c("lat", "lon"))
-extracted.pts.tave.m$value <- ifelse(extracted.pts.tave.m$value >= 1e+20, NA, extracted.pts.tave.m$value)
-ext.tave.sep <- extracted.pts.tave.m %>% tidyr::separate(variable, sep = "_", into = c("climate", "year", "month"))
-colnames(ext.tave.sep)[6] <- "Tave"
-
-
+rcp.8.5.index <- all.proj.names %>% filter(rcp %in% "rcp85") %>% select(proj.allnum)
+rcp.4.5.index <-  all.proj.names %>% filter(rcp %in% "rcp45") %>% select(proj.allnum)
+rcp.6.0.index <-  all.proj.names %>% filter(rcp %in% "rcp60") %>% select(proj.allnum)
 
 
+# then apply this funcation across all 195 projections downloaded in the netcdf
 
-saveRDS(ext.tave.sep, paste0("data/future_climate_tas_all/rdsfiles/july_tave_summary_proj_", proj, "_v1.rds"))
-rm(rast.stack.tave)
+
+#system.time(all.future.ppt <- lapply(1:2, FUN = extract.yearly.ppt)) 
+# user  system elapsed 
+# 11.062   0.749  12.938 
+
+# for loop with 2 :
+#   user  system elapsed 
+# 10.004   0.422  10.508 
+
+# for loop is slightly faster, so we will use that, but this takes a bit
+# extract by rcp (we could do all at once, but this splits it up)
+system.time(
+  for(i in unique(rcp.2.6.index$proj.allnum)){ # extracts for all 97 projections 
+    extract.tmax.june(proj = i,  tas = tave, tr_sites.lat.df = tr_sites.lat.df  , nmonths = nmonths)
+    print(i)
+  }
+)
+
+system.time(
+  for(i in unique(rcp.4.5.index$proj.allnum)){ 
+    extract.tmax.june(proj = i,  tas = tave, tr_sites.lat.df = tr_sites.lat.df  , nmonths = nmonths)
+    print(i)
+  }
+)
+
+system.time(
+  for(i in unique(rcp.6.0.index$proj.allnum)){ 
+    extract.tmax.june(proj = i,  tas = tave, tr_sites.lat.df = tr_sites.lat.df  , nmonths = nmonths)
+    print(i)
+  }
+)
+
+system.time(
+  for(i in unique(rcp.8.5.index$proj.allnum)){ 
+    extract.tmax.june(proj = i,  tas = tave, tr_sites.lat.df = tr_sites.lat.df  , nmonths = nmonths)
+    print(i)
+  }
+)
+
+
+#-------------------------------------------------------------------------
+# Read in the netcdf for total precipitation
+#-------------------------------------------------------------------------
+# get the filenames of the netcdfs:
+# note that the second file is max temp
+temp.files <- list.files("data/future_climate_tas_all/bcsd5/", pattern = ".nc")
+temp.ncs <- paste0(getwd(),"/data/future_climate_tas_all/bcsd5/", temp.files) # full filenames
+
+
+climate_output <- temp.ncs[1]
+
+nc <- nc_open(climate_output)
+variableofinterest <- names(nc$var) # get the variable of interest
+ppt <- ncvar_get(nc,variableofinterest) # this extracts a 4 dimensional array of data
+dim(tave)
+
+lon <- ncvar_get(nc, varid = "longitude") - 360
+lat <- ncvar_get(nc, varid = "latitude")
+lat <- ncvar_get(nc, varid = "latitude")
+#proj <- ncvar_get(nc, varid = "projection")
+time <- ncvar_get(nc, varid = "time")
+summary(360 - lon)
+summary(lat)
+nc$dim$time$units
+nc$dim$time$calendar
+
+#tas <- ncvar_get(nc, "tas") # pull out ta max
+#head(tas)
+#dim(tas)
+#colnames(tas) <- lon
+
+nc_close(nc)
+
+# dim(tave)# look at the dimensions
+nmonths <- dim(tppt)[3] # 3rd dimension is the number of months in the downscaled projections
+# ntave <- dim(tave)
+
+
+
+rlist <- list()
+
+proj <- 1
+
+tas <- ppt
+cov.data.ll <- tr_sites.lat.df
+
+extract.precipitation <- function(proj, tas,  tr_sites.lat.df, nmonths ){
+  # for the 1st model + projection, get raster lists of all the months
+  # make a raster for each month 
+  
+  
+  #years <-  c(rep(2025:2098, each = 12),2099)
+  
+  #months <- c(rep(1:12, 74), 1)
+  #ymonths <- 1:889
+  #julys <- ymonths[ months%in% "6"]
+  
+  for(m in 1:nmonths){ 
+    rlist[[m]] <- raster(as.matrix(t(tas[,101:1,m,proj])), xmn = min(lon), xmx = max(lon), 
+                         ymn = min(lat) , ymx = max(lat) , 
+                         crs = CRS('+init=epsg:4269'))
+  }
+  
+  rast.stack.tave <- stack(rlist)
+  
+  extracted.pts.tave <- data.frame(raster::extract(rast.stack.tave, tr_sites.lat.df[, c("coords.x1", "coords.x2")]))
+  
+  #ll.data <- as.data.frame(cov.data.ll)
+  extracted.pts.tave$lat <- tr_sites.lat.df$coords.x2 # get the lat and long
+  extracted.pts.tave$lon <- tr_sites.lat.df$coords.x1
+  
+  #system.time(removed.nas<- extracted.pts.tave[!rowSums(is.na(  extracted.pts.tave[cols])), ])
+  
+  # do the same for the extracted.pts.t
+  
+  
+  colnames(extracted.pts.tave)[1:889] <- c(paste0("tave_", rep(2025:2098,each = 12), "_", c(rep(1:12, 74)) ), "tavet_2099_1") # note may need to change this to make more customizable
+  extracted.pts.tave.m <- melt(extracted.pts.tave, id.vars = c("lat", "lon"))
+  extracted.pts.tave.m$value <- ifelse(extracted.pts.tave.m$value >= 1e+20, NA, extracted.pts.tave.m$value)
+  ext.tave.sep <- extracted.pts.tave.m %>% tidyr::separate(variable, sep = "_", into = c("climate", "year", "month"))
+  colnames(ext.tave.sep)[6] <- "Precip"
+  
+  
+  
+  
+  
+  saveRDS(ext.tave.sep, paste0("data/future_climate_tas_all/rdsfiles/yearly_precipitation_summary_proj_", proj, "_v1.rds"))
+  rm(rast.stack.tave)
 }
 
 
-extract.tmax.july(proj = 2, tas = tas, tr_sites.lat.df = tr_sites.lat.df, nmonths = nmonths)
+extract.precipitation(proj = 2, tas = ppt, tr_sites.lat.df = tr_sites.lat.df, nmonths = nmonths)
 
 
 all.proj.names <- read.delim("data/future_climate_tas_all/bcsd5/Projections5.txt", header = FALSE)
-proj.allnum<- 1:length(all.proj.names$V1)
+proj.allnum <- 1:length(all.proj.names$V1)
 # get the rcp values for all the projections
 all.proj.names$rcp <- substr(all.proj.names$V1, nchar(as.character(all.proj.names$V1))-4, nchar(as.character(all.proj.names$V1)))
 
@@ -190,32 +343,35 @@ all.future.ppt <- list()
 # 10.004   0.422  10.508 
 
 # for loop is slightly faster, so we will use that, but this takes a bit
+# extract by rcp (we could do all at once, but this splits it up)
 system.time(
-  for(i in unique(rcp.2.6.index)){ # extracts for all 97 projections 
-    extract.tmax.july(proj = i,  tas = tave, tr_sites.lat.df = tr_sites.lat.df  , nmonths = nmonths)
+  for(i in unique(rcp.2.6.index$proj.allnum)){ # extracts for all 97 projections 
+    extract.precipitation(proj = i,  tas = ppt, tr_sites.lat.df = tr_sites.lat.df  , nmonths = nmonths)
     print(i)
   }
 )
 
 system.time(
-  for(i in unique(rcp.4.5.index)){ # extracts for all 97 projections 
-    extract.tmax.july(proj = i,  tas = tave, tr_sites.lat.df = tr_sites.lat.df  , nmonths = nmonths)
+  for(i in unique(rcp.4.5.index$proj.allnum)){ 
+    extract.precipitation(proj = i,  tas = ppt, tr_sites.lat.df = tr_sites.lat.df  , nmonths = nmonths)
     print(i)
   }
 )
 
 system.time(
-  for(i in unique(rcp.6.0.index)){ # extracts for all 97 projections 
-    extract.tmax.july(proj = i,  tas = tave, tr_sites.lat.df = tr_sites.lat.df  , nmonths = nmonths)
+  for(i in unique(rcp.6.0.index$proj.allnum)){ 
+    extract.precipitation(proj = i,  tas = ppt, tr_sites.lat.df = tr_sites.lat.df  , nmonths = nmonths)
     print(i)
   }
 )
 
 system.time(
-  for(i in unique(rcp.8.5.index)){ # extracts for all 97 projections 
-    extract.tmax.july(proj = i,  tas = tave, tr_sites.lat.df = tr_sites.lat.df  , nmonths = nmonths)
+  for(i in unique(rcp.8.5.index$proj.allnum)){ 
+    extract.precipitation(proj = i,  tas = ppt, tr_sites.lat.df = tr_sites.lat.df  , nmonths = nmonths)
     print(i)
   }
 )
+
+
 
 
